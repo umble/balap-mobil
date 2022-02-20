8fe65bd316846393c99042700d6c14d962d7b51f
IKHWANUL-PC\Ikhwanul <ikhwanul.m.n@gmail.com>
2022-02-20 13:03:43 +0700

    test working car in cityGen3D

---
+++ Assets/CarSelectorScriptTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CarSelectorScriptTest : MonoBehaviour
+{
+    //public Scene sceneGameplay;
+    public GameObject SelectedCar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SelectCar(GameObject CarObject)
+    {
+        CarObject = SelectedCar;
+        Debug.Log(CarObject.name);
+        //SceneManager.LoadScene(sceneGameplay.name);
+    }
+}
